ace68f669cf09686e9ae21c31745318ddc639676
Sohaib Arif <arif_sohaib@outlook.com>
2019-07-10 14:06:26 -0400

    added router urls

--- GeniusRecipe/urls.py
+++ GeniusRecipe/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from recipe import views 
+
+routers = routers.DefaultRouter()
+routers.register(r'users', views.UserViewSet)
+routers.register(r'recipes', views.RecipeViewSet)
+routers.register(r'ingredients', views.IngredientViewSet)
+routers.register(r'steps', views.StepViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(routers.urls)), 
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('', include("recipe.urls")),
 ]
