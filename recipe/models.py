a05e3f9b24a9df862d9362c3af73d8d49cfaf0c2
Sohaib Arif <arif_sohaib@outlook.com>
2019-07-10 13:30:20 -0400

    added initial model and blank serializer

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+    name = models.CharField(max_length=150)
+    writer = models.OneToOneField('auth.User', related_name='recipes', on_delete=models.CASCADE)
+
+class Ingredient(models.Model):
+    recipe = models.ForeignKey(Recipe, related_name='ingredients', on_delete=models.CASCADE)
+    name = models.CharField(max_length=150)
+
+class Step(models.Model):
+    recpie = models.ForeignKey(Recipe, related_name='steps', on_delete=models.CASCADE)
+    step_text = models.TextField()
