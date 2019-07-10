4e86d1cafa929a4a5633b78385380506b8b5f2cd
Sohaib Arif <arif_sohaib@outlook.com>
2019-07-10 17:04:50 -0400

    com

--- recipe/serializers.py
+++ recipe/serializers.py
@@ -2,26 +2,27 @@ from rest_framework import serializers
 from recipe.models import Recipe, Ingredient, Step
 from django.contrib.auth.models import User
 
-class RecipeSerializer(serializers.HyperlinkedModelSerializer):
+class RecipeSerializer(serializers.ModelSerializer):
     writer = serializers.ReadOnlyField(source='writer.username')
-    ingredients = serializers.PrimaryKeyRelatedField(many=True, queryset=Ingredient.objects.all())
-    steps = serializers.PrimaryKeyRelatedField(many=True, queryset=Ingredient.objects.all())
+    recipes_ingredients = serializers.PrimaryKeyRelatedField(many=True, queryset=Ingredient.objects.all())
+    recipes_steps = serializers.PrimaryKeyRelatedField(many=True, queryset=Step.objects.all())
     class Meta:
         model = Recipe
-        fields = ('id','name', 'writer', 'ingridents', 'steps')
+        fields = ('url','id','name', 'writer', 'recipes_ingredients', 'recipes_steps')
+    
 
 class IngredientSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Ingredient
-        fields = ('id', 'name')
+        fields = ('url','id', 'name',)
 
 class StepSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Step
-        fields = ('id','step_text')
+        fields = ('url', 'id','step_text',)
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     recipes = serializers.PrimaryKeyRelatedField(many=True, queryset=Recipe.objects.all())
     class Meta:
         model = User 
-        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'recipes')
+        fields = ('url','id', 'username', 'first_name', 'last_name', 'email', 'recipes')
