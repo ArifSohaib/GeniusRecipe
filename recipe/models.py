1f9a175b270ff1a6d3f08cc272f1366b9f3ccdb7
Sohaib Arif <arif_sohaib@outlook.com>
2019-07-10 17:04:19 -0400

    fixed spelling error on foreign key

--- recipe/models.py
+++ recipe/models.py
@@ -2,16 +2,23 @@ from django.db import models
 
 class Recipe(models.Model):
     name = models.CharField(max_length=150, null=False, blank=False)
-    writer = models.OneToOneField('auth.User', related_name='recipes', on_delete=models.CASCADE)
+    writer = models.ForeignKey('auth.User', related_name='recipes', on_delete=models.CASCADE)
     def __str__(self):
         return f"{self.name} by {self.writer}"
+    def save(self, *args, **kwargs):
+        super(Recipe, self).save(*args, **kwargs)
 
 class Ingredient(models.Model):
-    recipe = models.ForeignKey(Recipe, related_name='ingredients', on_delete=models.CASCADE)
     name = models.CharField(max_length=150, null=False, blank=False)
+    recipie = models.ForeignKey(Recipe, related_name='recipes_ingredients', on_delete=models.CASCADE, null=True)
+
     def __str__(self):
         return f"{self.name}"
 
 class Step(models.Model):
-    recpie = models.ForeignKey(Recipe, related_name='steps', on_delete=models.CASCADE)
+    recipie = models.ForeignKey(Recipe, related_name='recipes_steps', on_delete=models.CASCADE, null=True)
     step_text = models.TextField(blank=False)
+    def __str__(self):
+        return f"{self.step_text}"
+
+
