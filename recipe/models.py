b6d22c0f79fd315735646c206c729e61ab967815
Sohaib Arif <arif_sohaib@outlook.com>
2019-07-10 13:36:09 -0400

    added required fields to recipe model

--- recipe/models.py
+++ recipe/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 class Recipe(models.Model):
-    name = models.CharField(max_length=150)
+    name = models.CharField(max_length=150, null=False, blank=False)
     writer = models.OneToOneField('auth.User', related_name='recipes', on_delete=models.CASCADE)
 
 class Ingredient(models.Model):
     recipe = models.ForeignKey(Recipe, related_name='ingredients', on_delete=models.CASCADE)
-    name = models.CharField(max_length=150)
+    name = models.CharField(max_length=150, null=False, blank=False)
 
 class Step(models.Model):
     recpie = models.ForeignKey(Recipe, related_name='steps', on_delete=models.CASCADE)
-    step_text = models.TextField()
+    step_text = models.TextField(blank=False)
